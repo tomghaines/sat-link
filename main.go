79e4cfcb856ac39b3897170d414da0c1daa572f4
tomghaines <tomhaines001@gmail.com>
2024-07-02 15:28:56 +0100

    Two-Line Element data structures & print functions set up

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type newSatellite struct {
 	name  string
@@ -8,7 +11,7 @@ type newSatellite struct {
 	line2 string
 }
 
-type TLEData struct {
+type lineOneTLE struct {
 	lineNumber             string
 	satelliteNumber        string
 	classification         string
@@ -25,6 +28,67 @@ type TLEData struct {
 	checksum               string
 }
 
+type lineTwoTLE struct {
+	lineNumber           string
+	satelliteNumber      string
+	inclination          string
+	rightAscensionOfNode string
+	eccentricity         string
+	argumentOfPerigee    string
+	meanAnomoly          string
+	meanMotion           string
+	revolutionNumAtEpoch string
+	checksum             string
+}
+
+func fmtLineOneTLE(sat newSatellite) lineOneTLE {
+
+	lineOneArr := strings.Split(sat.line1, "")
+
+	tle := lineOneTLE{
+		lineNumber:             "\nLine Number: " + strings.Join(lineOneArr[0:1], ""),
+		satelliteNumber:        "\nSatellite Number: " + strings.Join(lineOneArr[3:7], ""),
+		classification:         "\nClassifcation (U=Unclassified): " + strings.Join(lineOneArr[7:8], ""),
+		intDesLaunchYear:       "\nInternational Designator (Last two digits of launch year): " + strings.Join(lineOneArr[10:11], ""),
+		intDesLaunchYearNumber: "\nInternational Designator (Launch number of the year): " + strings.Join(lineOneArr[12:14], ""),
+		intDesLaunchPiece:      "\nInternational Designator (Piece of the launch): " + strings.Join(lineOneArr[15:17], ""),
+		epochYear:              "\nEpoch Year (Last two digits of year): " + strings.Join(lineOneArr[19:20], ""),
+		epochDay:               "\nEpoch (Day of the year and fractional portion of the day): " + strings.Join(lineOneArr[21:32], ""),
+		meanMotionFirst:        "\nFirst Time Derivative of the Mean Motion: " + strings.Join(lineOneArr[34:43], ""),
+		meanMotionSecond:       "\nSecond Time Derivative of Mean Motion (Leading decimal point assumed): " + strings.Join(lineOneArr[45:52], ""),
+		BSTAR:                  "\nBSTAR drag term (Leading decimal point assumed): " + strings.Join(lineOneArr[54:61], ""),
+		emphermerisType:        "\nEphemeris type: " + strings.Join(lineOneArr[62:63], ""),
+		elementNumber:          "\nElement Number: " + strings.Join(lineOneArr[65:68], ""),
+		checksum:               "\nChecksum (Modulo 10) - (Letters, blanks, periods, plus signs = 0; minus signs = 1): " + strings.Join(lineOneArr[68:69], ""),
+	}
+
+	return tle
+}
+
+func fmtLineTwoTLE(sat newSatellite) lineTwoTLE {
+
+	lineTwoArr := strings.Split(sat.line2, "")
+
+	tle := lineTwoTLE{
+		lineNumber:           "\nLine Number: " + strings.Join(lineTwoArr[0:1], ""),
+		satelliteNumber:      "\nSatellite Number: " + strings.Join(lineTwoArr[3:7], ""),
+		inclination:          "\nInclination [Degrees]: " + strings.Join(lineTwoArr[9:16], ""),
+		rightAscensionOfNode: "\nRight Ascension of the Ascending Node [Degrees]: " + strings.Join(lineTwoArr[18:25], ""),
+		eccentricity:         "\nEccentricity (Leading decimal point assumed): " + strings.Join(lineTwoArr[27:33], ""),
+		argumentOfPerigee:    "\nArgument of Perigee [Degrees]: " + strings.Join(lineTwoArr[35:42], ""),
+		meanAnomoly:          "\nMean Anomaly [Degrees]: " + strings.Join(lineTwoArr[44:51], ""),
+		meanMotion:           "\nMean Motion [Revs per day]: " + strings.Join(lineTwoArr[53:63], ""),
+		revolutionNumAtEpoch: "\nRevolution number at epoch [Revs]: " + strings.Join(lineTwoArr[64:68], ""),
+		checksum:             "\nChecksum: " + strings.Join(lineTwoArr[68:69], ""),
+	}
+
+	return tle
+}
+
+func printSatData(sat newSatellite) {
+	fmt.Printf("TLE Data: %v\nLine One: %v\nLine Two: %v", sat.name, fmtLineOneTLE(sat), fmtLineTwoTLE(sat))
+}
+
 func main() {
 
 	satelliteISS := newSatellite{
@@ -33,5 +97,12 @@ func main() {
 		line2: "2 25544  51.6432  21.5264 0002184  90.4728 285.4598 15.49212921247662",
 	}
 
-	fmt.Println(satelliteISS)
+	satelliteNOAA := newSatellite{
+		name:  "NOAA",
+		line1: "1 23455U 94089A   97320.90946019  .00000140  00000-0  10191-3 0  2621",
+		line2: "2 23455  99.0090 272.6745 0008546 223.1686 136.8816 14.11711747148495",
+	}
+
+	printSatData(satelliteNOAA)
+	printSatData(satelliteISS)
 }
